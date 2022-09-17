dd269ade165bb507e9eddee4746da3c08f6fa774
Angelo Bennett <77983302+ahbenn86@users.noreply.github.com>
2022-09-16 23:13:55 -0400

    created login & register pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,15 @@
 
-import './App.css';
+import 'antd/dist/antd.css';
+import {Button} from 'antd';
+
 
 function App() {
- 
+ return (
+    <div className='App p-5'>
+        <h1>This is the Doc Spot</h1>
+        <Button type="primary">Primary Button</Button>
+    </div>
+ );
 }
 
 export default App;
