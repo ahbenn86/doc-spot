2aea36f1e55cd16d12b0087a43d232c2c4e2c8db
Angelo Bennett <77983302+ahbenn86@users.noreply.github.com>
2022-09-20 19:00:40 -0400

    formatted & styled login & register pages

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import { Form, Input, Button } from "antd";
 
 function Login() {
+  const onFinish = (values) => {
+    console.log("Received values of form: ", values);
+  };
+
   return (
-    <div>Login</div>
-  )
+    <div className="section-wrapper">
+      <div className="logo-200 login"></div>
+      <div className="circle-logo">
+        <div className="auth-wrapper">
+          <div className="authentication login">
+            <div className="authentication-form card p-3">
+              <h1 className="card-title">Doc Spot Login</h1>
+              <Form layout="vertical" onFinish={onFinish}>
+                <Form.Item label="Email" name="email">
+                  <Input placeholder="Email" />
+                </Form.Item>
+                <Form.Item label="Password" name="password">
+                  <Input placeholder="Password" type="password" />
+                </Form.Item>
+                <Button className="primary-button my-3" htmlType="submit">
+                  LOGIN
+                </Button>
+                <Link to="/register" className="anchor login mt-2">
+                  CLICK HERE TO REGISTER
+                </Link>
+              </Form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,27 +1,42 @@
-import React from 'react';
-import {Form, Input, Button} from 'antd'
-
+import React from "react";
+import { Link } from "react-router-dom";
+import { Form, Input, Button } from "antd";
 
 function Register() {
+  const onFinish = (values) => {
+    console.log("Received values of form: ", values);
+  };
+
   return (
-    <div className='authentication'>
-      <div className='authentication-form card p-3'>
-        <h1 className='card-title'>Welcome to Doc Spot</h1>
-        <Form layout='vertical'>
-          <Form.Item label='Name' name='name'>
-            <Input placeholder='Name'/>
-          </Form.Item>
-          <Form.Item label='Email' name='email'>
-            <Input placeholder='Email'/>
-          </Form.Item>
-          <Form.Item label='Password' name='password'>
-            <Input placeholder='Password'/>
-          </Form.Item>
-          <Button className='primary-button mt-3'>REGISTER</Button>
-        </Form>
+    <div className="section-wrapper">
+      <div className="logo-200"></div>
+      <div className="circle-logo"></div>
+      <div className="auth-wrapper">
+      <div className="authentication">
+        <div className="authentication-form card p-3">
+          <h1 className="card-title">Welcome to Doc Spot</h1>
+          <Form layout="vertical" onFinish={onFinish}>
+            <Form.Item label="Name" name="name">
+              <Input placeholder="Name" />
+            </Form.Item>
+            <Form.Item label="Email" name="email">
+              <Input placeholder="Email" />
+            </Form.Item>
+            <Form.Item label="Password" name="password">
+              <Input placeholder="Password" type="password" />
+            </Form.Item>
+            <Button className="primary-button my-3" htmlType="submit">
+              REGISTER
+            </Button>
+            <Link to="/login" className="anchor mt-2">
+              CLICK HERE TO LOGIN
+            </Link>
+          </Form>
+        </div>
+      </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Register
+export default Register;
